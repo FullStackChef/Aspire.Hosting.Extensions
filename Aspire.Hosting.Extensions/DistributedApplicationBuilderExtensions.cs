0bb65af7a8764496834b0daeb29bc947c9d4b062
Brett Smith <brett@pukekos.co.nz>
2025-02-27 19:50:17 +1300

    missing file

---
+++ Aspire.Hosting.Extensions/DistributedApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Aspire.Hosting.Azure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aspire.Hosting;
+
+public static class DistributedApplicationBuilderExtensions
+{
+    public static IDistributedApplicationBuilder WithCustomNamingConvention(this IDistributedApplicationBuilder builder)
+    {
+        builder.AddParameter("stage");
+        builder.Services.Configure<AzureProvisioningOptions>(options =>
+            options.ProvisioningBuildOptions.InfrastructureResolvers.Insert(0,
+            new CustomResourceNamePropertyResolver(builder)));
+        return builder;
+    }
+}
